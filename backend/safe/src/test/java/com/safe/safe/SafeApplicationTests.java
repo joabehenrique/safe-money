7e9339d6f22d678e00f0d172def65ffc224d32cb
joabehenrique <henriquecidoz@hotmail.com>
2022-03-18 15:09:42 -0300

    feat: created psvm spring boot

---
+++ backend/safe/src/test/java/com/safe/safe/SafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safe.safe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
